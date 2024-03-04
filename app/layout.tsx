b0182201de2b8f8b0b97f9dfd48f3e3383213490
Anderson Leite <gtlt1999@gmail.com>
2024-03-03 22:37:45 -0300

    update vercel1

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'TikTok Clone',
-  description: 'TikTok Clone',
+  title: 'Educa Brasil',
+  description: 'Educa Brasil',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
